61a90a18191434b5e622919a94fbe4e5d093ae09
Mihai Blidaru <mihaiblidaru14@gmail.com>
2020-07-07 00:41:35 +0200

    Update limpiar.sh

--- limpiar.sh
+++ limpiar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "Deleting  auxiliary files"
 rm -f *.alg *.acr *.acn *.lof *.log *.lot *.out *.bak *.toc *.xdy *.ist *.gls *.glo *.blg *.aux *.bbl *.glg *.glsdefs main.run.xml main-blx.bib
-cd src/ && rm -f *.alg *.acr *.acn *.lof *.log *.lot *.out *.bak *.toc *.xdy *.ist *.gls *.glo *.blg *.aux *.bbl *.glg *.glsdefs  main.run.xml main-blx.bib && cd ..
+
